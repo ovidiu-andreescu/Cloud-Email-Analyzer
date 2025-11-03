ef99a028c86b125e53391f398f1517d83a8b66c1
Andrei <andreianghelea@gmail.com>
2025-11-03 20:53:08 +0200

    Added API endpoint page in ApiDocs.tsx. This involved introducing a routing system to handle navigation between the Activity page and the ApiDocs page. Kept App.tsx as the main layout for the application and moved its previous contents to Dashboard.tsx.
    Added a functional Show suspicious only checkbox in Dashboard page to filter for the emails with the Suspicious tag.
    
    Added a hover activated pop-up text for the tags of the emails (considered this feature helpful).

--- services/frontend/src/components/Badge.tsx
+++ services/frontend/src/components/Badge.tsx
@@ -1,10 +1,25 @@
 import React from "react"
 
 export default function Badge({kind, children}: {kind: "Safe"|"Unsafe"|"Suspicious", children?: React.ReactNode}) {
+
   const map = {
     Safe: "badge-safe",
     Unsafe: "badge-unsafe",
     Suspicious: "badge-suspicious"
   } as const
-  return <span className={"badge " + map[kind]}>{children ?? kind}</span>
+
+  const tooltipMap = {
+    Safe: "This email is safe. ",
+    Unsafe: "This email has been classified as unsafe and potentially malicious.",
+    Suspicious: "This email has suspicious content. Review needed."
+  }
+
+  return (
+    <span
+      className={"badge " + map[kind] + " cursor-help"}
+      title={tooltipMap[kind]}
+    >
+      {children ?? kind}
+    </span>
+  )
 }
